7b88f7a42c24d0137c25073d4d1c27972557e0c9
Matthew Brooks <mb964033@gmail.com>
2025-11-24 10:36:29 -0500

    update

--- make.lua
+++ make.lua
@@ -1,5 +1,5 @@
 local env = {
-  name = "lua-santoku-mustache",
+  name = "santoku-mustache",
   version = "0.0.1-1",
   license = "MIT",
   public = true,
@@ -22,6 +22,10 @@ local env = {
     }
   }
 }
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
   type = "lib",
