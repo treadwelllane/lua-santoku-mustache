aa84f842e4fb76baae897327be0b7ce4476eaa30
Matthew Brooks <mb964033@gmail.com>
2025-12-10 13:47:39 -0500

    update

--- make.lua
+++ make.lua
@@ -1,11 +1,11 @@
 local env = {
   name = "santoku-mustache",
-  version = "0.0.13-1",
+  version = "0.0.14-1",
   license = "MIT",
   public = true,
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.303-1",
+    "santoku >= 0.0.310-1",
   },
   cflags = {
     "-I$(shell luarocks show santoku --rock-dir)/include/",
@@ -33,6 +33,4 @@ env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
-return {
-  env = env,
-}
+return { env = env }
